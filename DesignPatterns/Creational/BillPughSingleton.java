7698d0c91fe031c6af91547d6269c97b7fe5a6f8
Jay Krishna Korrapati <78617436+JaarkarS@users.noreply.github.com>
2024-05-28 13:27:57 -0400

    Add files via upload

---
+++ DesignPatterns/Creational/BillPughSingleton.java
@@ -0,0 +1,21 @@
+package DesignPatterns.CreationalPattern;
+
+public class BillPughSingleton {
+
+    private BillPughSingleton() {}
+
+    private static class SingletonHelper {
+        private static final BillPughSingleton returnSingleInstance = new BillPughSingleton();
+    }
+
+    public static  BillPughSingleton getInstance(){
+           return SingletonHelper.returnSingleInstance;
+    }
+
+    public static void main(String[] args){
+        System.out.println(BillPughSingleton.getInstance());
+        System.out.println(BillPughSingleton.getInstance());
+        System.out.println(BillPughSingleton.getInstance());
+
+    }
+}

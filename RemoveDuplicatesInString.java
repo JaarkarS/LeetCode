2692dbcbc35d9c771f65c5490644728ecece5155
Jay Krishna Korrapati <78617436+JaarkarS@users.noreply.github.com>
2024-05-28 21:01:18 -0400

    Add files via upload

---
+++ RemoveDuplicatesInString.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveDuplicatesInString {
+    public static void main(String args[]){
+        String str = "jaykorrapati";
+        System.out.println(removeDuplicates(str));
+    }
+
+    private static String removeDuplicates(String str) {
+        if(str == null) {
+            throw new IllegalArgumentException("Input is null");
+        }
+        StringBuilder strb =  new StringBuilder();
+        char[] chararray = str.toCharArray();
+        Map charExists = new HashMap();
+        for(int i = 0; i< chararray.length; i++){
+            if(!charExists.containsKey(chararray[i])){
+                strb.append(chararray[i]);
+                charExists.put(chararray[i],1);
+            }
+
+        }
+
+        return strb.toString();
+    }
+}
